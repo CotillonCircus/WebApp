cec6cb5b0a104780c694ffe35144ede2b4c0ab26
XDrimer <tutemaposo2016@gmail.com>
2022-09-08 20:08:47 -0300

    Auth0 implementado

--- client/src/index.js
+++ client/src/index.js
@@ -5,15 +5,18 @@ import App from './App';
 import store from "./redux/store";
 import {Provider} from "react-redux";
 import {BrowserRouter} from "react-router-dom";
+import Auth0ProviderWithHistory from './components/auth0-provider-with-history/auth0-provider-with-history';
 
 const container = document.getElementById("root")
 const root = ReactDOMClient.createRoot(container);
 
 root.render(
   <BrowserRouter>
+    <Auth0ProviderWithHistory>
         <Provider store={store}>
           <App />
         </Provider>
+    </Auth0ProviderWithHistory>
   </BrowserRouter>
   );
 
