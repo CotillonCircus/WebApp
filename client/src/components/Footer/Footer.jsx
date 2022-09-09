4c313d31b678f97b3a9524e341e723f9572c30a7
Gonzalo Gomez <gonzalogdv@gmail.com>
2022-09-09 01:15:21 -0300

    Footer, Header, Navbar en Home

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <div class='container-fluid text-center'>
+        <div class='row'>
+          <div class='col-lg'>Footer</div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
