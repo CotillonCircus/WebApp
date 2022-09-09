b3bfbd309b9641432f7f1fec360b01e345b4bd4b
XDrimer <tutemaposo2016@gmail.com>
2022-09-09 19:05:40 -0300

    Navbar y links, contact form variable

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import {useAuth0} from "@auth0/auth0-react";
+import "./Navbar.css";
+import {Link} from "react-router-dom";
 
 const Navbar = () => {
+
+  const { isAuthenticated, user } = useAuth0();
+
   return (
     <nav className='navbar navbar-expand-md bg-light'>
       <div className='container-fluid'>
@@ -17,11 +23,15 @@ const Navbar = () => {
           <span className='navbar-toggler-icon'></span>
         </button>
         <div className='collapse navbar-collapse' id='navbarNavDropdown'>
-          <ul className='navbar-nav'>
+          {
+            isAuthenticated ? (
+              <ul className='navbar-nav'>
             <li className='nav-item'>
-              <a className='nav-link active' aria-current='page' href='#header'>
-                Inicio
-              </a>
+              <Link to={"/home"} className="link">
+                <a className='nav-link active' aria-current='page' href='#header' id='as'>
+                  Inicio
+                </a>
+              </Link>
             </li>
 
             <li className='nav-item dropdown'>
@@ -53,16 +63,96 @@ const Navbar = () => {
               </ul>
             </li>
             <li className='nav-item'>
-              <a className='nav-link' href='#'>
-                Quienes Somos
-              </a>
+              <Link to="/about" className="link">
+                <a className='nav-link' href='#'>
+                  Quienes Somos
+                </a>             
+              </Link>
             </li>
             <li className='nav-item'>
-              <a className='nav-link' href='#footer'>
-                Contacto
-              </a>
+              <Link to={"/contact"} className="link">
+                <a className='nav-link' href='#footer'>
+                  Contacto
+                </a>             
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to={"/tutorial"} className="link">
+                <a className='nav-link' href='#footer'>
+                  Tutorial
+                </a>
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to={"/aform"} className="link">
+                <a className='nav-link' href='#footer'>
+                  Formulario Autorizacion
+                </a>
+              </Link>
             </li>
           </ul>
+            ) : (
+              <ul className='navbar-nav'>
+              <li className='nav-item'>
+                <Link to={"/home"} className="link">
+                  <a className='nav-link active' aria-current='page' href='#header' id='as'>
+                    Inicio
+                  </a>
+                </Link>
+              </li>
+  
+              <li className='nav-item dropdown'>
+                <a
+                  className='nav-link dropdown-toggle'
+                  href='#nav'
+                  role='button'
+                  data-bs-toggle='dropdown'
+                  aria-expanded='false'
+                >
+                  Productos
+                </a>
+                <ul className='dropdown-menu'>
+                  <li>
+                    <a className='dropdown-item' href='#'>
+                      Globos
+                    </a>
+                  </li>
+                  <li>
+                    <a className='dropdown-item' href='#'>
+                      Cortinas
+                    </a>
+                  </li>
+                  <li>
+                    <a className='dropdown-item' href='#'>
+                      Banderines
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li className='nav-item'>
+                <Link to="/about" className="link">
+                  <a className='nav-link' href='#'>
+                    Quienes Somos
+                  </a>             
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link to={"/contact"} className="link">
+                  <a className='nav-link' href='#footer'>
+                    Contacto
+                  </a>             
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link to={"/tutorial"} className="link">
+                  <a className='nav-link' href='#footer'>
+                    Tutorial
+                  </a>
+                </Link>
+              </li>
+            </ul>
+            )
+          }
         </div>
       </div>
     </nav>
---
+++ client/src/pages/AForm/AForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AForm = () => {
+  return (
+    <div>AForm</div>
+  )
+}
+
+export default AForm
