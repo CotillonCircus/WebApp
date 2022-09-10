eb64a91ef02f8bdc2e11d64e4994628bd42189ef
XDrimer <tutemaposo2016@gmail.com>
2022-09-09 21:02:37 -0300

    navbar en todas las paginas

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Footer from '../../components/Footer/Footer';
+import Header from '../../components/Header/Header';
+import Navbar from '../../components/Navbar/Navbar';
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <Header />
+      <Navbar />
+      <Footer />
+    </div>
   )
 }
 
