8f0f4baf11c648db5718b1d3d07c836739dac7b8
patricio colella <patriciogabrielcolella@gmail.com>
2022-09-12 15:47:27 -0300

    product details zones marked

--- client/src/components/DetailsProduct/DetailsProduct.jsx
+++ client/src/components/DetailsProduct/DetailsProduct.jsx
@@ -4,7 +4,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import { getProductById } from '../../redux/actions'
 import "./detailsProduct.css"
-import {FaFacebookF,FaTwitter,FaPinterestP} from "react-icons/fa"
+import {FaFacebookF,FaTwitter,FaPinterestP,FaCcVisa,FaCcMastercard,FaCreditCard} from "react-icons/fa"
+import {SiAmericanexpress} from "react-icons/si"
 
 const img = "https://d2r9epyceweg5n.cloudfront.net/stores/001/156/993/products/fucsia1-9304b057a0c7c113d415886127367996-1024-1024.jpg"
 
@@ -30,18 +31,33 @@ const DetailsProduct = () => {
       <div id="detailsContainer">
         <div id="leftDetails">
           <div id="productImg">
-            <img src={img} alt="" />
+            <img src={productDetails.img} alt="" />
           </div>
           <div id="mediaIcons">
-            <span><FaFacebookF></FaFacebookF></span>
-            <span><FaTwitter></FaTwitter></span>
-            <span><FaPinterestP></FaPinterestP></span>
+            <span><FaFacebookF/></span>
+            <span><FaTwitter/></span>
+            <span><FaPinterestP/></span>
           </div>
         </div>
         <div id="rigthDetails">
           <div id="productDetails">
-            <span id='productName'>MANTEL TELA VEGETAL FUCSIA 1,20X1,80M</span>
-            <span id="productPrice">$329,90</span>
+            <span id='productName'>{productDetails.name} {productDetails.color} {productDetails.size||24}" X{productDetails.cant||20}</span>
+            <span id="productPrice">${productDetails.price}</span>
+          </div>
+          <div id="payWays">
+            <div>
+              <span className="bold">24 </span><span>CUOTAS DE</span><span className="bold"> ${productDetails.price/24}</span>
+            </div>
+            <div id="payIcons">
+              <FaCcVisa className="payIcon"/>
+              <FaCcMastercard className="payIcon"/>
+              <SiAmericanexpress className="payIcon"/>
+              <FaCreditCard className="payIcon"/>
+            </div>
+            <span>VER MEDIOS DE PAGO</span>
+          </div>
+          <div id="payForm">
+              formulario de pago
           </div>
         </div>
       </div>
