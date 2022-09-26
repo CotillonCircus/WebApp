add17cf0555a43f73f3821959cbb1292834f7840
Gonzalo Gomez <gonzalogdv@gmail.com>
2022-09-26 15:10:12 -0300

    Mercado pago actions and controller

--- api/src/controllers/paymentsControllers.js
+++ api/src/controllers/paymentsControllers.js
@@ -1,1 +1,51 @@
+const mercadopago = require('mercadopago');
+const dotenv = require('dotenv');
 
+dotenv.config();
+
+// REPLACE WITH YOUR ACCESS TOKEN AVAILABLE IN: https://developers.mercadopago.com/panel
+mercadopago.configure({
+  access_token: process.env.ACCESS_TOKEN,
+});
+
+const createPreference = async (req, res, next) => {
+  console.log(req.body);
+  let preference = {
+    items: [
+      {
+        title: req.body.title,
+        unit_price: Number(req.body.unit_price),
+        quantity: Number(req.body.quantity),
+      },
+    ],
+    back_urls: {
+      success: 'http://localhost:3000/success',
+      failure: 'http://localhost:3000/failure',
+      pending: 'http://localhost:3000/pending',
+    },
+    auto_return: 'approved',
+  };
+
+  mercadopago.preferences
+    .create(preference)
+    .then(function (response) {
+      console.log(response.body.init_point);
+      res.json({
+        id: response.body.id,
+        init_point: response.body.init_point,
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
+
+const getFeedback = async (req, res, next) => {
+  res.json({
+    Payment: req.query.payment_id,
+    Status: req.query.status,
+    MerchantOrder: req.query.merchant_order_id,
+  });
+};
+
+module.exports = { createPreference, getFeedback };
--- api/src/routes/paymentRoutes.js
+++ api/src/routes/paymentRoutes.js
@@ -1,52 +1,12 @@
 const { Router } = require('express');
+const {
+  createPreference,
+  getFeedback,
+} = require('../controllers/paymentsControllers');
+
 const router = Router();
 
-const mercadopago = require('mercadopago');
-const dotenv = require('dotenv');
-
-dotenv.config();
-
-// REPLACE WITH YOUR ACCESS TOKEN AVAILABLE IN: https://developers.mercadopago.com/panel
-mercadopago.configure({
-  access_token: process.env.ACCESS_TOKEN,
-});
-
-router.post('/create_preference', (req, res) => {
-  let preference = {
-    items: [
-      {
-        title: req.body.title,
-        unit_price: Number(req.body.unit_price),
-        quantity: Number(req.body.quantity),
-      },
-    ],
-    back_urls: {
-      success: 'http://localhost:3000/feedback',
-      failure: 'http://localhost:3000/feedback',
-      pending: 'http://localhost:3000/feedback',
-    },
-    auto_return: 'approved',
-  };
-
-  mercadopago.preferences
-    .create(preference)
-    .then(function (response) {
-      res.json({
-        id: response.body.id,
-        init_point: response.body.init_point,
-      });
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-});
-
-router.get('/feedback', function (req, res) {
-  res.json({
-    Payment: req.query.payment_id,
-    Status: req.query.status,
-    MerchantOrder: req.query.merchant_order_id,
-  });
-});
+router.post('/create_preference', createPreference);
+router.get('/feedback', getFeedback);
 
 module.exports = router;
