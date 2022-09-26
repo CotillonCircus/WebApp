a9642d0c9e6edbcc17b3e216a554f54bb8ede7e2
Gonzalo Gomez <gonzalogdv@gmail.com>
2022-09-26 09:02:57 -0300

    mercado pago checkout pro

--- api/src/routes/paymentRoutes.js
+++ api/src/routes/paymentRoutes.js
@@ -1,18 +1,52 @@
 const { Router } = require('express');
 const router = Router();
 
-const PaymentController = require('../controllers/paymentsControllers');
-const PaymentService = require('../services/paymentsService');
-const PaymentInstance = new PaymentController(new PaymentService());
+const mercadopago = require('mercadopago');
+const dotenv = require('dotenv');
 
-router.get('/', function (req, res, next) {
-  return res.json({
-    '/single': 'generates a single payment link',
-  });
+dotenv.config();
+
+// REPLACE WITH YOUR ACCESS TOKEN AVAILABLE IN: https://developers.mercadopago.com/panel
+mercadopago.configure({
+  access_token: process.env.ACCESS_TOKEN,
 });
 
-router.get('/single', function (req, res, next) {
-  PaymentInstance.getPaymentLink(req, res);
+router.post('/create_preference', (req, res) => {
+  let preference = {
+    items: [
+      {
+        title: req.body.title,
+        unit_price: Number(req.body.unit_price),
+        quantity: Number(req.body.quantity),
+      },
+    ],
+    back_urls: {
+      success: 'http://localhost:3000/feedback',
+      failure: 'http://localhost:3000/feedback',
+      pending: 'http://localhost:3000/feedback',
+    },
+    auto_return: 'approved',
+  };
+
+  mercadopago.preferences
+    .create(preference)
+    .then(function (response) {
+      res.json({
+        id: response.body.id,
+        init_point: response.body.init_point,
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
+router.get('/feedback', function (req, res) {
+  res.json({
+    Payment: req.query.payment_id,
+    Status: req.query.status,
+    MerchantOrder: req.query.merchant_order_id,
+  });
 });
 
 module.exports = router;
--- api/src/routes/routes.js
+++ api/src/routes/routes.js
@@ -2,20 +2,20 @@ const { Router, static } = require('express');
 const path = require('path');
 const router = Router();
 
-const productRoutes = require("./productRoutes")
-const userRoutes = require("./userRoutes")
-const catalogRoutes = require("./catalogRoutes")
-const authRoutes = require("./authRoutes")
-const cloudinaryRoutes = require("./cloudinaryRoutes")
+const productRoutes = require('./productRoutes');
+const userRoutes = require('./userRoutes');
+const catalogRoutes = require('./catalogRoutes');
+const authRoutes = require('./authRoutes');
+const cloudinaryRoutes = require('./cloudinaryRoutes');
 const paymentRoutes = require('./paymentRoutes');
 
 router.use('/images', static(path.join(__dirname, 'images')));
 
-router.use("/product",productRoutes)
-router.use("/user",userRoutes)
-router.use("/catalog",catalogRoutes)
-router.use("/auth",authRoutes)
-router.use("/cloudinary",cloudinaryRoutes)
+router.use('/product', productRoutes);
+router.use('/user', userRoutes);
+router.use('/catalog', catalogRoutes);
+router.use('/auth', authRoutes);
+router.use('/cloudinary', cloudinaryRoutes);
 router.use('/payment', paymentRoutes);
 
 module.exports = router;
