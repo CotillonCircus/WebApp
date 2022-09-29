1ba3674744a52ccac4822bd2fec0dcd13f155bc5
Gonzalo Gomez <gonzalogdv@gmail.com>
2022-09-29 15:41:17 -0300

    DetailProduct

--- client/src/adminComponents/AuthList/AuthCard/AuthCard.jsx
+++ client/src/adminComponents/AuthList/AuthCard/AuthCard.jsx
@@ -1,44 +1,55 @@
 import React from 'react';
-import styles from "./AuthCard.module.css";
+import styles from './AuthCard.module.css';
 import { useDispatch } from 'react-redux';
 import { changeUserStatus } from '../../../redux/actions';
 import axios from 'axios';
 
-const AuthCard = ({id,sub,name,email,company,cuit,address,razon_social,setAuthFlag}) => {
+const AuthCard = ({
+  id,
+  sub,
+  name,
+  email,
+  company,
+  cuit,
+  address,
+  razon_social,
+  setAuthFlag,
+}) => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const authorize = (e) => {
+    e.preventDefault();
+    dispatch(changeUserStatus(sub, 'mayorista'));
+    axios
+      .delete('http://localhost:3001/auth', { data: { id } })
+      .then((res) => console.log('Usuario autorizado'));
+    setAuthFlag((prevFlag) => !prevFlag);
+  };
 
-    const authorize = (e)=>{
-        e.preventDefault();
-        dispatch(changeUserStatus(sub,"mayorista"));
-        axios.delete("http://localhost:3001/auth",{data: {id}})
-        .then(res=>console.log("Usuario autorizado"));
-        setAuthFlag((prevFlag)=>!prevFlag);
-    }
-
-    const deny = (e)=>{
-        e.preventDefault();
-        dispatch(changeUserStatus(sub,"unauthorized"));
-        axios.delete("http://localhost:3001/auth",{data: {id}})
-        .then(res=>console.log("Usuario denegado"));
-        setAuthFlag((prevFlag)=>!prevFlag);
-    }
+  const deny = (e) => {
+    e.preventDefault();
+    dispatch(changeUserStatus(sub, 'unauthorized'));
+    axios
+      .delete('http://localhost:3001/auth', { data: { id } })
+      .then((res) => console.log('Usuario denegado'));
+    setAuthFlag((prevFlag) => !prevFlag);
+  };
 
   return (
     <div className={styles.authCardContainer}>
-        <p>ID Usuario: {sub}</p>
-        <p>Nombre: {name}</p>
-        <p>Email: {email}</p>
-        <p>Compania: {company}</p>
-        <p>Cuit: {cuit}</p>
-        <p>Dirección: {address}</p>
-        <p>Razón social: {razon_social}</p>
-        <div className={styles.buttonsContainer}>
-            <button onClick={(e)=>authorize(e)}>Autorizar</button>
-            <button onClick={(e)=>deny(e)}>Denegar</button>
-        </div>
+      <p>ID Usuario: {sub}</p>
+      <p>Nombre de contacto: {name}</p>
+      <p>Email: {email}</p>
+      <p>Companía: {company}</p>
+      <p>Cuit: {cuit}</p>
+      <p>Domicilio de entrega: {address}</p>
+      <p>Razón social: {razon_social}</p>
+      <div className={styles.buttonsContainer}>
+        <button onClick={(e) => authorize(e)}>Autorizar</button>
+        <button onClick={(e) => deny(e)}>Denegar</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AuthCard
+export default AuthCard;
--- client/src/components/DetailsProduct/DetailsProduct.jsx
+++ client/src/components/DetailsProduct/DetailsProduct.jsx
@@ -1,33 +1,46 @@
-import { useAuth0 } from '@auth0/auth0-react';
+//import { useAuth0 } from '@auth0/auth0-react';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { getProductById, getRelated } from '../../redux/actions';
+import { getProductById, getRelated, getProductos } from '../../redux/actions';
 import './detailsProduct.css';
-import {
-  FaFacebookF,
-  FaTwitter,
-  FaPinterestP,
-  FaCcVisa,
-  FaCcMastercard,
-  FaCreditCard,
-} from 'react-icons/fa';
-import { SiAmericanexpress } from 'react-icons/si';
+// import {
+//   FaFacebookF,
+//   FaTwitter,
+//   FaPinterestP,
+//   FaCcVisa,
+//   FaCcMastercard,
+//   FaCreditCard,
+// } from 'react-icons/fa';
+// import { SiAmericanexpress } from 'react-icons/si';
 import { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import { useShoppingCart } from '../Context/ShoppingCartContext';
 
 const DetailsProduct = () => {
   const { productDetails } = useSelector((state) => state);
   const dispatch = useDispatch();
   const { ID } = useParams();
   const [related, setRelated] = useState([]);
-  const user = useSelector((state)=>state.userLogged[0]);
+  const user = useSelector((state) => state.userLogged[0]);
+  const {
+    getItemQuantity,
+    increaseCartQuantity,
+    decreaseCartQuantity,
+    removeFromCart,
+  } = useShoppingCart();
+  const { productos } = useSelector((state) => state);
 
   useEffect(() => {
     ID && dispatch(getProductById(ID));
   }, [dispatch, ID]);
 
   useEffect(() => {
-    productDetails.name&&getRelated(productDetails.name,setRelated)
+    dispatch(getProductos({}));
+  }, [dispatch]);
+
+  useEffect(() => {
+    productDetails.name && getRelated(productDetails.name, setRelated);
   }, [productDetails]);
 
   // async function x() {
@@ -37,13 +50,13 @@ const DetailsProduct = () => {
   return (
     productDetails && (
       <div id='detailsPage'>
-        <span>direccion</span>
+        {/* <span>direccion</span> */}
         <div id='detailsContainer'>
           <div id='leftDetails'>
             <div id='productImg'>
-              <img src={productDetails.img} alt='' />
+              <img src={productDetails.img} alt='productDetails.img' />
             </div>
-            <div id='mediaIcons'>
+            {/* <div id='mediaIcons'>
               <span>
                 <FaFacebookF />
               </span>
@@ -53,7 +66,7 @@ const DetailsProduct = () => {
               <span>
                 <FaPinterestP />
               </span>
-            </div>
+            </div> */}
           </div>
           <div id='rigthDetails'>
             <div id='productDetails'>
@@ -62,14 +75,58 @@ const DetailsProduct = () => {
                 {productDetails.size || 24}" X{productDetails.cant || 20}
               </span>
               <span id='productPrice'>
-                {
-                    user ? user.status === "mayorista" || user.status === "admin" ? (
-                      <span> ${productDetails.price}</span>
-                    ) : (<span>Se necesita autorizacion para ver los precios</span>) : (<span>Registrate y autorizate para ver los precios</span>)
-                  } 
-                </span>
+                {user ? (
+                  user.status === 'mayorista' || user.status === 'admin' ? (
+                    <span> ${productDetails.price}</span>
+                  ) : (
+                    <span>Se necesita autorizacion para ver los precios</span>
+                  )
+                ) : (
+                  <span>Registrate y autorizate para ver los precios</span>
+                )}
+              </span>
             </div>
-            <div id='payWays'>
+            <div id='cartcontrols'>
+              {productDetails && (
+                <div
+                  className='d-flex align-items-center flex-column'
+                  style={{ gap: '0.5rem' }}
+                  key={productDetails.id}
+                >
+                  <div
+                    className='d-flex align-items-center justify-content-center'
+                    style={{ gap: '0.5rem' }}
+                  >
+                    <Button
+                      onClick={() => decreaseCartQuantity(productDetails.id)}
+                      className='btn-secondary'
+                    >
+                      -
+                    </Button>
+                    <div>
+                      <span className='fs-3'>
+                        {getItemQuantity(productDetails.id)}
+                      </span>{' '}
+                      en carrito
+                    </div>
+                    <Button
+                      onClick={() => increaseCartQuantity(productDetails.id)}
+                      className='btn-secondary'
+                    >
+                      +
+                    </Button>
+                  </div>
+                  <Button
+                    variant='danger'
+                    size='sm'
+                    onClick={() => removeFromCart(productDetails.id)}
+                  >
+                    Quitar
+                  </Button>
+                </div>
+              )}
+            </div>
+            {/* <div id='payWays'>
               <div>
                 <span className='bold'>24 </span>
                 <span>CUOTAS DE</span>
@@ -88,14 +145,17 @@ const DetailsProduct = () => {
               </div>
               <span>VER MEDIOS DE PAGO</span>
             </div>
-            <div id='payForm'>formulario de pago</div>
+            <div id='payForm'>formulario de pago</div> */}
           </div>
         </div>
-        <div id='facebookcoments'>facebookcoments</div>
+        {/* <div id='facebookcoments'>facebookcoments</div> */}
         <div id='relacionados'>
-          {related?.slice(0, 4).map((rel) => {
-            return <img src={rel.img} alt={rel} key={rel.id} />;
-          })}
+          {related
+            ?.filter((rel) => rel.id !== productDetails.id)
+            .slice(0, 4)
+            .map((rel) => {
+              return <img src={rel.img} alt={rel} key={rel.id} />;
+            })}
         </div>
       </div>
     )
