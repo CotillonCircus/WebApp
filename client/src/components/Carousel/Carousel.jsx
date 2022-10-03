e43721a4ba4a2dca33a567730a7aff874b881949
Gonzalo Gomez <gonzalogdv@gmail.com>
2022-10-03 15:27:40 -0300

    CSS Meeting

--- client/src/components/Carousel/Carousel.jsx
+++ client/src/components/Carousel/Carousel.jsx
@@ -26,14 +26,14 @@ const Carousel = () => {
           type='button'
           data-bs-target='#carouselExampleIndicators'
           data-bs-slide-to='0'
-          className='off'
+          className='active'
           aria-current='true'
           aria-label='Slide 1'
         ></button>
         <button
           type='button'
           data-bs-target='#carouselExampleIndicators'
-          className='active'
+          className='off'
           data-bs-slide-to='1'
           aria-label='Slide 2'
         ></button>
