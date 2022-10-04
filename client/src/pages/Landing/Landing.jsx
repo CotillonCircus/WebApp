f70ffe523b08e6fa158ef11c12e27044a4dd30bf
XDrimer <tutemaposo2016@gmail.com>
2022-10-04 20:05:53 -0300

    Landing css

--- client/src/pages/Landing/Landing.jsx
+++ client/src/pages/Landing/Landing.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import style from "./Landing.module.css";
 import { Link } from "react-router-dom";
+import Logo from '../../images/circus_cotillon_logo.jpg';
 
 const Landing = () => {
   return (
     <div className={style.container}>
+      <div className='col-lg-6 logo'>
+              <img src={Logo} className='d-block w-50 mx-auto' alt='logo' />
+      </div>
       <div className={style.buttonContainer}>
         <button className={style.button}>Minorista</button>
           <Link to={"/home"}>
