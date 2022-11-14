3d3a7bbf288eb64a8195e3cc31570b83cc379cef
Gonzalo Gomez <gonzalogdv@gmail.com>
2022-11-14 15:30:14 -0300

    Node_env v1/api

--- api/src/app.js
+++ api/src/app.js
@@ -17,7 +17,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', "*"); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header(
     'Access-Control-Allow-Headers',
@@ -27,7 +27,7 @@ server.use((req, res, next) => {
   next();
 });
 
-server.use('/', routes);
+server.use('/v1/api', routes);
 
 // Error catching endware.
 server.use((err, req, res, next) => {
